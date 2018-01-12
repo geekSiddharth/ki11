822f7306c3bff8483c6c7446bb19d58b50b02ae3
geekSiddharth <siddharth16268@iiitd.ac.in>
2018-01-12 09:24:38 +0530

    cat.c added

---
+++ cat.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    if (argc <= 1) {
+        printf("One argument is required");
+        return 1;
+    }
+
+    FILE *fileptr;
+    fileptr = fopen(argv[1], "r");
+
+    if (fileptr) {
+        //if file is opened
+
+        // prints everything character by character
+        char c;
+        while ((c = (char) fgetc(fileptr)) != EOF) {
+            printf("%c", c);
+        }
+        fclose(fileptr);
+    } else {
+        printf("Unable to read the file or it doesn't exists \"%s\" \n", argv[1]);
+    }
+
+}
