0ae827e5bc36420e054f2659807c615faebe4573
geekSiddharth <siddharth16268@iiitd.ac.in>
2018-01-12 09:11:19 +0530

    ls made

---
+++ ls.c
@@ -0,0 +1,31 @@
+#include <dirent.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+
+    DIR *directory;
+
+    if(argc>1) {
+        // case in which argument is sullied
+        directory = opendir(argv[1]);
+    } else {
+        //no argument case or empty argument case
+        directory = opendir(".");
+    }
+
+    if (directory) {
+        // if we successfully read te directory
+
+        struct dirent *structdirent;
+
+        while ((structdirent = readdir(directory)) != NULL) {
+            printf("%s\n", structdirent->d_name);
+        }
+
+        closedir(directory);
+    } else {
+        printf("\"%s\" is not directory or it was not found\n", argv[1]);
+    }
+    return(0);
+}
