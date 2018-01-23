448b122e0394f17d05709c2527e0b70f70d43415
geekSiddharth <siddharth16268@iiitd.ac.in>
2018-01-23 19:50:04 +0530

    history made

--- main.c
+++ main.c
@@ -11,10 +11,18 @@
 FILE *file;
 char prevpath[LINE_LIMIT];
 
+
 int init_shell() {
-    using_history();
+//    using_history();
+
     printf("Welcome to ki11\n\n");
-    file = fopen(".ki11", "a+");
+    char *c = getenv("HOME");
+    file = fopen(strcat(c, "/ki11"), "a+");
+    return 0;
+}
+
+int echo(char *line) {
+    printf("%s",line+1);
     return 0;
 }
 
@@ -78,11 +86,66 @@ int pwd() {
     return -1;
 }
 
-void print_history() {
-    fseek(file,0,SEEK_SET);
-    char c;
-    while ((c = (char) fgetc(file)) != EOF) {
-        printf("%c", c);
+int print_history(int argc, char **argv) {
+
+    int tail = 0;
+    if (argc > 1) {
+
+        // clear history
+        if (strcmp(argv[1], "-c") == 0) {
+            ftruncate(fileno(file), 0);
+            fprintf(file,"history -c\n");
+            return 0;
+        }
+
+        // read content from a file and append it to this history
+        if (strcmp(argv[1], "-a") == 0) {
+
+            if (argc < 3) {
+                perror("ERROR: Mention the file name to read from");
+                return -1;
+            }
+
+            FILE *toopen = fopen(argv[2], "r");
+            fseek(file, 0, SEEK_END);
+            if (toopen) {
+                char c;
+                while ((c = (char) fgetc(toopen)) != EOF) {
+                    fprintf(file, "%c", c);
+                }
+                fclose(toopen);
+            } else {
+                perror("ERROR: ");
+                return -1;
+            }
+            return 0;
+        }
+
+        // last option for printing last x lines
+        tail = atoi(argv[1]);
+        int seek_val = 2;
+        fseek(file, 0, SEEK_END);
+        int end = (int) ftell(file);
+
+        while (tail != 0 &&  seek_val<end) {
+
+            fseek(file, end - seek_val, SEEK_SET);
+            if (fgetc(file) == '\n') {
+                tail--;
+            }
+            seek_val++;
+        }
+
+    } else {
+        fseek(file, 0, SEEK_SET);
+    }
+
+    char * line_read = NULL;
+    size_t len = 0;
+    int counter=1;
+
+    while (getline(&line_read, &len, file) != -1) {
+        printf("%d\t%s", counter++, line_read);
     }
 
 //
@@ -109,10 +172,11 @@ int execute(int argc, char **argv) {
     } else if (strcmp(argv[0], "echo") == 0) {
 
     } else if (strcmp(argv[0], "history") == 0) {
-        print_history();
+        print_history(argc, argv);
         return 0;
     } else {
         // do fork and stuffs
+
     }
 
     return 1;
@@ -141,21 +205,27 @@ int main(void) {
         //reads lines
         fgets(line, LINE_LIMIT, stdin);
 
-        if(strlen(line)>1) {
+
+        if (strlen(line) > 3 && line[0] == 'e' && line[1] == 'c' && line[2] == 'h' && line[3] == 'o') {
+            echo(line + 4);
+            continue;
+        }
+
+        if (strlen(line) > 1) {
             //maintaining the history file
             fprintf(file, "%s", line);
             fflush(file);
 
 //            add_history(line);
+
         }
+
+
 
         //for handling empty lines
         if ((argv[0] = strtok(line, " \t\n")) == NULL) {
             continue;
         }
-
-
-
 
         // now breaking input line and breaking it into tokens
         argc = 1;
